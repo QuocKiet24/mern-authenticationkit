bf743fd3e0e7da57712f64adbb00f2610baedbbd
trquockiet24 <trquockiet24@gmail.com>
2024-12-03 15:55:11 +0700

    default role admin

--- server/src/models/auth/user.model.js
+++ server/src/models/auth/user.model.js
@@ -32,7 +32,7 @@ const userSchema = new mongoose.Schema(
     role: {
       type: String,
       enum: ["admin", "user", "creator"],
-      default: "creator",
+      default: "admin",
     },
     isVerified: {
       type: Boolean,
